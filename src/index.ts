de16d10032d79dc7071af06e7836ca2a429b9669
Adam Gipril <adam.gipril@icloud.com>
2019-09-26 20:46:43 -0600

    Hook up POST route, create & submit events to Google Calendar

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import * as express from 'express';
-
-const port: string = process.env.PORT || '4003';
+import calendar from './google/calendar';
 
 const app = express();
-const server = app.listen(port);
+const server = app.listen(process.env.PORT || '4003');
+
+app.use(express.json());
+app.post('/schedule', ({ body }, res) => {
+  try {
+    const { 'start-date': start, schedule } = body;
+    calendar.createEvents(start, schedule)
+      .then(calendar.submitEvents)
+      .then(() => res.sendStatus(201))
+      .catch(() => res.sendStatus(500));
+  } catch (err) {
+    res.sendStatus(400);
+  }
+});
 
 export { app, server };
