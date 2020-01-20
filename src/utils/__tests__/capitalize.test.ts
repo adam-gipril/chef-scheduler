659cbf3a98f859a1f3e18aed52964a7813fddcf6
Adam Gipril <adam.gipril@icloud.com>
2020-01-19 21:58:58 -0700

    Reword test titles for capitalize

--- src/utils/__tests__/capitalize.test.ts
+++ src/utils/__tests__/capitalize.test.ts
@@ -4,11 +4,11 @@ describe('util: capitalize', () => {
   const string = 'my BEEFY haunches';
   const capitalized = capitalize(string);
 
-  it('capitalizes the first letter of a string', () => {
+  it('capitalizes the first character of a string', () => {
     expect(capitalized[0]).toBe('M');
   });
 
-  it('converts every other letter to lowercase', () => {
+  it('converts every other character to lowercase', () => {
     const [, ...rest] = capitalized;
     rest.forEach(char => expect(char).toBe(char.toLowerCase()));
   });
