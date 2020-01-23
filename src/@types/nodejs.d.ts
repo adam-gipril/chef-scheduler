0cc238316a24df63341ed3bf79dd5b186f1abfeb
Adam Gipril <adam.gipril@icloud.com>
2020-01-22 23:50:13 -0700

    Make module alias work on built code

--- src/@types/nodejs.d.ts
+++ src/@types/nodejs.d.ts
@@ -1,5 +1,8 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
+    /** Credentials to authenticate as a Google service account */
     readonly CREDENTIALS: string;
+    /** Port at which the express app will listen */
+    readonly PORT: string;
   }
 }
