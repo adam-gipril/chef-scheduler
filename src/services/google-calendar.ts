3e1f11126f11eaed6d0ef66fb033bc4741d3d3b6
Adam Gipril <adam.gipril@icloud.com>
2020-01-22 21:04:22 -0700

    Remove return value from addEvents

--- src/services/google-calendar.ts
+++ src/services/google-calendar.ts
@@ -20,7 +20,6 @@ export default class GoogleCalendarService {
    *
    * @param {Event[]} events array of events to be inserted
    * @param {string} [calendarId=primary] (optional) calendar to which events will be added
-   * @returns {Promise<GaxiosResponse<calendar_v3.Schema$Event>[]>} array of responses
    */
   static async addEvents(events: Event[], calendarId = 'primary') {
     const auth = await this.getJWT();
@@ -32,6 +31,6 @@ export default class GoogleCalendarService {
         requestBody: event,
       }),
     );
-    return Promise.all(requests);
+    await Promise.all(requests);
   }
 }
