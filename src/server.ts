aa755b405c777f6b156d34f89777a5c90a05611a
Adam Gipril <adam.gipril@icloud.com>
2020-01-23 00:55:20 -0700

    Add test coverage script, upgrade deps

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,7 @@ import 'module-alias/register';
 import app from '@/app';
 
 const port = process.env.PORT || '4003';
-const server = app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  if (process.env.NODE_ENV === 'development') console.log(`listening on ${port}`);
-});
+// eslint-disable-next-line no-console
+const server = app.listen(port, () => console.log(`listening on ${port}`));
 
 export default server;
