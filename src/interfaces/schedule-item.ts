6ffe5620771a98013ad1055750d8e39d1fb5f2ce
Adam Gipril <adam.gipril@icloud.com>
2020-01-25 07:19:26 -0700

    Add comments/doc to interfaces

--- src/interfaces/schedule-item.ts
+++ src/interfaces/schedule-item.ts
@@ -1,6 +1,9 @@
 /** Interface for schedule entries submitted to the chef-cal-integration API. */
 export interface ScheduleItem {
+  /** Type of meal the chef will cook */
   type: 'main' | 'side';
+  /** Name of person cooking */
   chef: string;
+  /** Day of the week on which cooking event falls (three-letter abbreviation) */
   day: string;
 }
