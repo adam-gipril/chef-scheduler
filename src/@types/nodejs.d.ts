035659b90dca283274a8dd5e8ecca4fee8772e80
Adam Gipril <adam.gipril@icloud.com>
2020-02-05 20:46:30 -0700

    Add declaration of PEOPLE environment variable
    The PEOPLE environment variable will be used to securely store our
    contact information until a DB solution becomes necessary.

--- src/@types/nodejs.d.ts
+++ src/@types/nodejs.d.ts
@@ -6,6 +6,8 @@ declare namespace NodeJS {
     readonly AUTH_TOKEN: string;
     /** Phone number from which chef-cal-integration SMS messages will be sent */
     readonly HOST_NUMBER: string;
+    /** Array of people used until a DB solution becomes necessary */
+    readonly PEOPLE: string;
     /** Credentials to authenticate as a Google service account */
     readonly CREDENTIALS: string;
     /** Current runtime environment */
