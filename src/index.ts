e1c734684d5f9aa489482bfcc5c1b4d0bafeb199
Adam Gipril <adam.gipril@icloud.com>
2020-02-22 13:26:58 -0700

    Add log output before scheduling task

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 /* istanbul ignore file */
 import 'module-alias/register';
+import moment from 'moment';
 import { schedule as createTask } from 'node-cron';
 import { main } from '@/tasks';
 
 if (process.env.NODE_ENV === 'production') {
+  /* eslint-disable-next-line */
+  console.log(`${moment().format()}: scheduling task "main"`);
   createTask('* * * * Friday', main);
 }
